08ae3bfa40cd7b433f1d7be48f4c133534dc6dfa
Usama Ejaz <uejaz0@gmail.com>
2022-08-24 21:14:13 +0400

    Add transformer

--- app.py
+++ app.py
@@ -1,17 +1,27 @@
 from flask import Flask, jsonify
+from transformers import DistilBertTokenizerFast
+from transformers import TFDistilBertForSequenceClassification
+import os
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
 
 app = Flask(__name__)
 
 
-@app.route('/monday/<task>', methods=['GET'])
-def analyze_task(task):
+newmodel = TFDistilBertForSequenceClassification.from_pretrained("model1")
+new_tokenizer =DistilBertTokenizerFast.from_pretrained('tokenizer1')
 
+
+@app.route('/out/<task>', methods=['GET'])
+def analyze_task(task):
     return jsonify({'Test': task})
 
 
 @app.route('/', methods=['GET'])
 def main():
-    return jsonify({'Test': "Working"})
+    print("Inside main")
+    predict_input = new_tokenizer.encode("climate change is man made concept", return_tensors="tf")
+    return jsonify({'Test': str(predict_input)})
 
 
 if __name__ == "__main__":
