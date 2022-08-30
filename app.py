ab7aab32adc3f2bc483752a8be101b82753c4e0c
Usama Ejaz <uejaz0@gmail.com>
2022-08-30 15:41:04 +0400

    detele model

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ from transformers import TFDistilBertForSequenceClassification
 app = Flask(__name__)
 
 
-newmodel = TFDistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased',  num_labels=3)
-new_tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+# newmodel = TFDistilBertForSequenceClassification.from_pretrained('distilbert-base-uncased',  num_labels=3)
+# new_tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
 
 
 @app.route('/out/<task>', methods=['GET'])
@@ -20,8 +20,8 @@ def analyze_task(task):
 @app.route('/', methods=['GET'])
 def main():
     print("Inside main")
-    predict_input = new_tokenizer.encode("climate change is man made concept", return_tensors="tf")
-    return jsonify({'Test': str(predict_input)})
+    # predict_input = new_tokenizer.encode("climate change is man made concept", return_tensors="tf")
+    return jsonify({'Test': "Fuck u"})
 
 
 if __name__ == "__main__":
