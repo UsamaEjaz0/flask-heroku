b89ca8b5c6e8d46f00e4b36f63cc335e164ae443
Usama Ejaz <uejaz0@gmail.com>
2022-09-02 17:01:22 +0400

    replaced distilbert variation

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask, jsonify
-from transformers import DistilBertTokenizerFast
-from transformers import TFDistilBertForSequenceClassification
+# from transformers import DistilBertTokenizerFast
+# from transformers import TFDistilBertForSequenceClassification
+
+from transformers import pipeline
 # import os
 # os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 app = Flask(__name__)
+classifier = pipeline("text-classification",model='distilbert-base-uncased-finetuned-sst-2-english', return_all_scores=True)
 
-newmodel = TFDistilBertForSequenceClassification.from_pretrained('updated_model',  num_labels=3)
-new_tokenizer = DistilBertTokenizerFast.from_pretrained('tokenizer')
+# newmodel = TFDistilBertForSequenceClassification.from_pretrained('updated_model',  num_labels=3)
+# new_tokenizer = DistilBertTokenizerFast.from_pretrained('tokenizer')
 
 
 @app.route('/out/<task>', methods=['GET'])
@@ -18,8 +21,9 @@ def analyze_task(task):
 @app.route('/', methods=['GET'])
 def main():
     print("Inside main")
-    predict_input = new_tokenizer.encode("climate change is man made concept", return_tensors="tf")
-    return jsonify({'Test': "Fuck you"})
+    # predict_input = new_tokenizer.encode("climate change is man made concept", return_tensors="tf")
+    prediction = classifier("Climate change is real", )
+    return jsonify({'Test': str(prediction)})
 
 
 if __name__ == "__main__":
